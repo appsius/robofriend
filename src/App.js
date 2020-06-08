3efeda0752f568d08c89800aa9a4bc24af1d70fb
appsius <ilyas.ustunkaya234434@gmail.com>
2020-06-08 18:31:45 +0300

    React App Part-4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: '',
 		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -23,14 +28,17 @@ class App extends Component {
 				.toLowerCase()
 				.includes(this.state.searchField.toLowerCase());
 		});
-		console.log(filteredRobots);
-		return (
-			<div className='tc'>
-				<h1 className='f2'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots} />
-			</div>
-		);
+		if (this.state.robots.length === 0) {
+			return <h1 className='tc'>Loading</h1>;
+		} else {
+			return (
+				<div className='tc'>
+					<h1 className='f2'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<CardList robots={filteredRobots} />
+				</div>
+			);
+		}
 	}
 }
 
